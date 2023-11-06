c389e53836383de77f0e5ba701b262c047d044a2
hushujiang <260068900@qq.com>
2023-11-06 12:36:53 +0800

    1

--- 读取文件2.py
+++
@@ -1,6 +0,0 @@
-from pathlib import Path  # pathlib is recommended
-
-#repofile = Path('../repo_file.txt')  # 读取上一级目录文件
-repofile = Path('./repo_file.txt')  # 读取上一级目录文件
-with repofile.open() as f: 
-    print(f.readline())
